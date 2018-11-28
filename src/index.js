470f07f7c8f0d532e3c775a36dde60c30fad0f4d
Jacob Juul Christiansen <jacob.juul@gmail.com>
2018-11-28 10:02:25 +0100

    Split render into functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import Tabs from "./tabs";
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Tabs from './tabs'
 
-import "./styles.scss";
+import './styles.scss'
 
 function App() {
-  return <Tabs selectedSection="section1" />;
+  return <Tabs selectedSection="section1" />
 }
 
-const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+const rootElement = document.getElementById('root')
+ReactDOM.render(<App />, rootElement)
--- src/tabs.js
+++ src/tabs.js
@@ -1,88 +1,132 @@
-import React, { Component } from 'react';
-import cx from 'classnames';
-import data from './data';
+import React, { Component, Fragment } from 'react'
+import cx from 'classnames'
+import data from './data'
 
-const TABS_LEXINO = 'LEXINO';
-const TABS_KOMMENTAR = 'KOMMENTARER';
-
-// { state: {
-//  activeSectionId: 'section1',
-//  activeTab: 'tab_lexino',
-//  normalizeBySections: {
-//    section1: {
-//      tab_lexino: {
-//        loadingState: 'loading',
-//        data: []
-//      },
-//      tab_kommentarer: {
-//        loadingState: 'loaded',
-//        data: []
-//      }
-//    },
-//    section2: {
-//      tab_lexino: {
-//        loadingState: 'error',
-//        data: []
-//      }
-//    }
-//  }
-// } }
+const TABS_LEXINO = 'LEXINO'
+const TABS_KOMMENTAR = 'KOMMENTARER'
 
 export default class Tabs extends Component {
   constructor(props) {
-    super(props);
+    super(props)
     this.state = {
+      dropdownActive: false,
       activeSectionId: props.selectedSection,
       activeTab: props.initialTab || TABS_KOMMENTAR,
       normalizeBySections: data
-    };
+    }
 
-    this.handleLexinoClicked = this.handleTabClick.bind(this, TABS_LEXINO);
+    this.handleLexinoClicked = this.handleTabClick.bind(this, TABS_LEXINO)
     this.handleKommentarerClicked = this.handleTabClick.bind(
       this,
       TABS_KOMMENTAR
-    );
+    )
   }
 
   renderContent = () => {
-    const { normalizeBySections, activeSectionId } = this.state;
+    const { normalizeBySections, activeSectionId } = this.state
     switch (normalizeBySections[activeSectionId][this.state.activeTab].name) {
       case TABS_LEXINO:
-        return <div>Lexino</div>;
+        return <div>Lexino</div>
 
       default:
-        return <div>Loading</div>;
+        return <div>Loading...</div>
     }
-  };
+  }
+
+  renderTabs = () => {
+    const { activeTab } = this.state
+    return (
+      <Fragment>
+        <div
+          onClick={this.handleKommentarerClicked}
+          className={cx('c-tabs__tab-button', {
+            'c-tabs__tab-button--active': activeTab === TABS_KOMMENTAR
+          })}
+        >
+          Lag kommentarer
+        </div>
+        <div
+          onClick={this.handleLexinoClicked}
+          className={cx('c-tabs__tab-button', {
+            'c-tabs__tab-button--active': activeTab === TABS_LEXINO
+          })}
+        >
+          Lexino
+        </div>
+        <div
+          onClick={this.handleLexinoClicked}
+          className={cx('c-tabs__tab-button', {})}
+        >
+          Lexino
+        </div>
+        <div
+          onClick={this.handleLexinoClicked}
+          className={cx('c-tabs__tab-button', {})}
+        >
+          Lexino
+        </div>
+        <div
+          onClick={this.handleLexinoClicked}
+          className={cx('c-tabs__tab-button', {})}
+        >
+          Lexino
+        </div>
+        <div
+          onClick={this.handleLexinoClicked}
+          className={cx('c-tabs__tab-button', {})}
+        >
+          Lexino
+        </div>
+        <div
+          onClick={this.handleLexinoClicked}
+          className={cx('c-tabs__tab-button', {})}
+        >
+          Lexino
+        </div>
+      </Fragment>
+    )
+  }
+
+  renderDropdown = () => {
+    return (
+      <Fragment>
+        <div
+          className="c-tabs__tab-dropdown-toggle"
+          onClick={this.toggleDropdown}
+        >
+          V
+        </div>
+        <div
+          className={cx('c-tabs__tab-dropdown-box', {
+            'c-tabs__tab-dropdown-box--hidden': !this.state.dropdownActive
+          })}
+        />
+      </Fragment>
+    )
+  }
 
   handleTabClick = tabName => {
-    this.setState({ activeTab: tabName });
-  };
+    this.setState({ activeTab: tabName })
+  }
+
+  toggleDropdown = () => {
+    this.setState({ dropdownActive: !this.state.dropdownActive })
+  }
+
+  closeDropdown = () => {
+    if (this.state.dropdownActive) {
+      this.setState({ dropdownActive: false })
+    }
+  }
 
   render() {
-    const { activeTab } = this.state;
+    const { activeTab } = this.state
     return (
-      <div className="c-tabs">
-        <div className="c-tabs__tab-buttons">
-          <div
-            onClick={this.handleKommentarerClicked}
-            className={cx('c-tabs__tab-button', {
-              'c-tabs__tab-button--active': activeTab === TABS_KOMMENTAR
-            })}
-          >
-            Lag kommentarer
-          </div>
-          <div
-            onClick={this.handleLexinoClicked}
-            className={cx('c-tabs__tab-button', {
-              'c-tabs__tab-button--active': activeTab === TABS_LEXINO
-            })}
-          >
-            Lexino
-          </div>
-        </div>
-        {this.renderContent()}
+      <div className="c-tabs" onClick={this.closeDropdown}>
+        <div className="c-tabs__tab-buttons">{this.renderTabs()}</div>
+        <div className="c-tabs__tab-dropdown">{this.renderDropdown()}</div>
+        <div className="c-tabs__tab-content">{this.renderContent()}</div>
       </div>
-    );
+    )
   }
 }
